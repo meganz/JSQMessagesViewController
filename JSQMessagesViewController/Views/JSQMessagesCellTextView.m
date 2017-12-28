6c550b23a5bd8240b54ad1d9c4688af8402d8fe6
Javier Trujillo <jth@mega.nz>
2017-12-28 11:12:36 +0100

    Disabled URL dragging in iOS 11 to allow long press to copy/share/open them

--- JSQMessagesViewController/Views/JSQMessagesCellTextView.m
+++ JSQMessagesViewController/Views/JSQMessagesCellTextView.m
@@ -98,3 +98,45 @@
 }
 
 @end
+
+// Hack to disable the draggability on UITextViews
+
+#import <objc/runtime.h>
+
+@implementation UIDragInteraction (TextLimitations)
++ (void)load
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        Class class = [self class];
+        
+        SEL originalSelector = @selector(isEnabled);
+        SEL swizzledSelector = @selector(restrictIsEnabled);
+        
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        
+        BOOL didAddMethod =
+        class_addMethod(class,
+                        originalSelector,
+                        method_getImplementation(swizzledMethod),
+                        method_getTypeEncoding(swizzledMethod));
+        
+        if (didAddMethod) {
+            class_replaceMethod(class,
+                                swizzledSelector,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+}
+
+-(BOOL)restrictIsEnabled
+{
+    return NO;
+}
+
+@end
